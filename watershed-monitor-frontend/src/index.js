8b8f842d3b659a06d0bacffbffadf745a0896e1c
jessesbyers <jesse.s.byers@gmail.com>
2020-03-03 15:37:02 -0500

    test success - front-end and back-end communication

--- watershed-monitor-frontend/src/index.js
+++ watershed-monitor-frontend/src/index.js
@@ -4,7 +4,7 @@ console.log("testing...")
 
 
 // test that we can get data from the backend____________________________________
-const BACKEND_URL = 'localhost:3000';
+const BACKEND_URL = 'http://localhost:3000';
 fetch(`${BACKEND_URL}/test`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
