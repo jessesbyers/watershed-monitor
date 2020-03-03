48b9682ca5f2f2d4ea147463a0b618ed8d246bc6
jessesbyers <jesse.s.byers@gmail.com>
2020-03-03 10:55:39 -0500

    testing communication btw frontend and backend

--- watershed-monitor-backend/app/controllers/application_controller.rb
+++ watershed-monitor-backend/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::API
-end
+# class ApplicationController < ActionController::API
+# end
+
+# TEST ____________________________________________
+class ApplicationController < ActionContoller::API
+    def test
+      render json: { test: "success" }
+    end
+  end
--- watershed-monitor-backend/config/routes.rb
+++ watershed-monitor-backend/config/routes.rb
@@ -1,3 +1,9 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
+
+
+# TEST _________________________________________
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/test', to: 'application#test'
 end
