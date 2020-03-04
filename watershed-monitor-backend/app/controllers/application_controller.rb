c83d7b226f8d59adb79116f5e3cac320cd0c04ec
jessesbyers <jesse.s.byers@gmail.com>
2020-03-04 08:32:16 -0500

    test fetch of seed data to index.js

--- watershed-monitor-backend/app/controllers/application_controller.rb
+++ watershed-monitor-backend/app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
-# class ApplicationController < ActionController::API
-# end
-
-# TEST ____________________________________________
 class ApplicationController < ActionController::API
-    def test
-      render json: { test: "success" }
-    end
-  end
+end
+
+
--- watershed-monitor-frontend/src/index.js
+++ watershed-monitor-frontend/src/index.js
@@ -1,7 +1,16 @@
-console.log("testing...")
+//   test that we can fetch seed data from the backend_________________________________
+fetch(`${BACKEND_URL}/maps`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
 
-// test that we can get data from the backend____________________________________
-const BACKEND_URL = 'http://localhost:3000';
-fetch(`${BACKEND_URL}/test`)
+fetch(`${BACKEND_URL}/maps/1`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
+
+fetch(`${BACKEND_URL}/observations`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
+
+fetch(`${BACKEND_URL}/observations/1`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
