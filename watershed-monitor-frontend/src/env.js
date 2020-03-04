287ad5e91f8a98f8aefe7859a10fae115f721094
jessesbyers <jesse.s.byers@gmail.com>
2020-03-04 14:23:20 -0500

    minor styling to make map fit on styles.css

--- watershed-monitor-frontend/src/env.js
+++ watershed-monitor-frontend/src/env.js
@@ -2,7 +2,7 @@
 
 const API_KEY = "AIzaSyAv4WPYmC60DsKf3lkbaTfDVP9KzA9Ny9I"
 
-function setApi() = {
+function setApi() {
     let api = document.querySelector("script#api")
     api.setAttribute("src", `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`)
     console.log(API_KEY)
