aafc7112df8711e9375782515d32b1d3949431e8
jessesbyers <jesse.s.byers@gmail.com>
2020-03-04 08:35:48 -0500

    added backend URL const to index.js

--- watershed-monitor-frontend/src/index.js
+++ watershed-monitor-frontend/src/index.js
@@ -1,4 +1,5 @@
 //   test that we can fetch seed data from the backend_________________________________
+const BACKEND_URL = "http://localhost:3000"
 fetch(`${BACKEND_URL}/maps`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
