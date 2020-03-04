bc07b3b9790d663b7f0609009bc8b058b5675e89
jessesbyers <jesse.s.byers@gmail.com>
2020-03-04 12:03:59 -0500

    added comments for troubleshooting key

--- watershed-monitor-frontend/src/env.js
+++ watershed-monitor-frontend/src/env.js
@@ -1,8 +1,10 @@
+// not working yet
+
 const API_KEY = "AIzaSyAv4WPYmC60DsKf3lkbaTfDVP9KzA9Ny9I"
 
-document.addEventListener(DOMContentLoaded, function() { 
-
+function setApi() = {
     let api = document.querySelector("script#api")
     api.setAttribute("src", `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`)
     console.log(API_KEY)
-})
+    console.log(api)
+}
