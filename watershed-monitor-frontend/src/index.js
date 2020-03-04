3cd5812131f7a49334f7d6b4bbdcd22fba755374
jessesbyers <jesse.s.byers@gmail.com>
2020-03-04 09:30:43 -0500

    initMap function and script set on index.js and index.html

--- watershed-monitor-frontend/src/index.js
+++ watershed-monitor-frontend/src/index.js
@@ -1,3 +1,14 @@
+//   intimap function initiates rendering of the map on the DOM
+function initMap() {
+    // placeholder for center of map for testing - eventually replace with data from Map object fetched from backend
+    // let mapCenter =  { lat: 44.811345, lng: 73.149572} 
+        let mapCenter =  { lat: 44.8107, lng: -73.0836} 
+
+    let map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: mapCenter});
+    let marker = new google.maps.Marker({position: mapCenter, map: map});
+}
+
+
 //   test that we can fetch seed data from the backend_________________________________
 const BACKEND_URL = "http://localhost:3000"
 fetch(`${BACKEND_URL}/maps`)
@@ -15,3 +26,4 @@ fetch(`${BACKEND_URL}/observations`)
 fetch(`${BACKEND_URL}/observations/1`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
+
