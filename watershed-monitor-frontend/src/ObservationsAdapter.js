1c2a68ad67bcb1a85ab1abf75063076d10f57bb2
jessesbyers <jesse.s.byers@gmail.com>
2020-03-12 09:43:19 -0400

    added comments to ObservationsAdapter.js

--- watershed-monitor-frontend/src/ObservationsAdapter.js
+++ watershed-monitor-frontend/src/ObservationsAdapter.js
@@ -11,6 +11,8 @@ class ObservationsAdapter {
             .then(json => {
                 let observations = json.data
 
+                // creates an observation object from each entry in the database from json data
+                // calls renderMarker function on each observation object to place observations on the map
                 observations.forEach(obs => {
                     let observation = new Observation(obs.id, obs.attributes.name, obs.attributes.description, obs.attributes.category_id, obs.attributes.latitude, obs.attributes.longitude)
                     observation.renderMarker(map)
@@ -22,7 +24,7 @@ class ObservationsAdapter {
 
     // ********************FETCH (POST) TO PERSIST NEW OBSERVATION TO DATABASE**********************************
     // addMarkerToDatabase function called in ShowNewObservationForm function
-    // sends a post request to backend to create new observation instance from formData and persist it in the database
+    // sends a post request to backend to create new observation instance from newObservation object and persist it in the database
     addMarkerToDatabase(newObservation, map) {
 
         let configObj = {
@@ -53,26 +55,23 @@ class ObservationsAdapter {
 
 // *************FETCH (DELETE) CALL TO DELETE OBSERVATION FROM DATABASE AND REMOVE FROM MAP*********************
     removeObsFromDatabase(marker) {
-        console.log(marker)
         let id = parseInt(marker.label.text)
-    
+        
+        // removes double click event listeners from markers
         markersArray.map(marker => {
             google.maps.event.clearListeners(marker, 'dblclick')
-    })
+        })
     
         let configObj = {
             method: "DELETE",
             headers: 
-            {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-            },
+                {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+                },
         };
-    
+        
         fetch(`${this.baseURL}/${id}`, configObj) 
-        .then(function(response) {
-            // response.json();
-        })
         .then(function(json) {
             marker.setVisible(false)
             marker.setMap(null)
