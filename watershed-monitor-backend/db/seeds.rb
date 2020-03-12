ad42a78f1693f60c6974d568121867b2d14ceab9
jessesbyers <jesse.s.byers@gmail.com>
2020-03-12 08:23:39 -0400

    revised seed data and reset database

--- watershed-monitor-backend/db/seeds.rb
+++ watershed-monitor-backend/db/seeds.rb
@@ -1,6 +1,6 @@
 obs1 = {
     name: "Bedard Farm",
-    description: "Manure spread on fields after allowed date",
+    description: "Manure spread on fields over snow after allowed date",
     latitude: 44.811345,
     longitude: -73.149572,
     category_id: 1
@@ -8,41 +8,41 @@ obs1 = {
 
 obs2 = {
     name: "SweetPea Gardens",
-    description: "Cover crops used extensively",
+    description: "Cover crops used extensively during fall and spring seasons",
     latitude: 44.819235,
     longitude: -73.137827,
     category_id: 2
 }
 
 obs3 = {
-    name: "test3",
-    description: "fake water quality data",
+    name: "Hathaway Point",
+    description: "Toxic Blue-Green Algae Bloom - no swimming allowed",
     latitude: 44.808884,
     longitude: -73.169820,
     category_id: 3
 }
 
 obs4 = {
-    name: "test4",
-    description: "cows grazing in floodplain",
-    latitude: 45.007420,
-    longitude: -73.091199,
+    name: "Stevens Brook",
+    description: "Cows grazing in floodplain without fencing",
+    latitude: 44.802736381572714,
+    longitude: -73.11476287841796,
     category_id: 1
 }
 
 obs5 = {
-    name: "test5",
-    description: "Stormwater diverted and contained in catchment system",
-    latitude: 44.958852,
-    longitude: -73.164499,
+    name: "Private Residence",
+    description: "Stormwater diverted and contained in water catchment system",
+    latitude: 44.845837509069824,
+    longitude: -73.10240325927734,
     category_id: 2
 }
 
 obs6 = {
-    name: "test6",
-    description: "fake water quality data",
-    latitude: 44.935890, 
-    longitude: -73.196943,
+    name: "Private Well",
+    description: "Nitrate level significantly above safe drinking water level",
+    latitude: 44.82270814193098,
+    longitude: -73.07699737548828,
     category_id: 3
 }
 
